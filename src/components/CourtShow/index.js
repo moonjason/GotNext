64ca9483859b1f484e450419e9db496785625ea7
Jason Moon <jasonmoonjm@gmail.com>
2019-12-05 01:09:02 -0800

    add map marker showpage

--- src/components/CourtShow/index.js
+++ src/components/CourtShow/index.js
@@ -1,37 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { Marker } from 'react-map-gl'
+
 
 const CourtShow = (props) => {
-    const [court, setCourt] = useState([]);
+    const [court, setCourt] = useState(null);
     const [viewport, setViewport] = useState({
         width: 400,
         height: 400,
-        zoom: 10,
+        zoom: 14,
     });
 
     useEffect(() => {
         const getCourt = async () => {
-            const courtId = props.match.params.court
-            const court = await fetch(`/api/v1/courts/show/${courtId}`);
+            const court = await fetch(`/api/v1/courts/show/${props.match.params.court}`);
             const parsedCourt = await court.json();
-            console.log(parsedCourt)
-            setCourt(parsedCourt);
             setViewport({latitude: parsedCourt.coordinates.latitude, longitude: parsedCourt.coordinates.longitude, ...viewport})
+            setCourt(parsedCourt);
         }
-        getCourt();
-    }, [])
+        getCourt()
+    }, []);
 
-    // async componentDidMount() {
-    //     const movieId = this.props.match.params.id;
-    //     const resMovie = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`);
-    //     const movieJson = await resMovie.json();
-    //     console.log(movieJson);
-    // }
-
-    console.log(viewport)
-    console.log(court)
-    return (
-        <>
+    return (    
+        <> 
+            {console.log(court)}
+            {  
+                court
+                ?
+                <>
             <h1>{court.name}</h1>
             <img style={{"maxWidth": "22rem", "maxHeight": "22rem"}} src={court.image_url} alt=""/>
             <ReactMapGL 
@@ -41,7 +36,17 @@ const CourtShow = (props) => {
                 onViewportChange={viewport => {
                     setViewport(viewport);
                 }}
-            />
+            >
+                <Marker
+                    latitude={court.coordinates.latitude}
+                    longitude={court.coordinates.longitude}
+                >
+                    <div>Hi</div>
+                </Marker>
+            </ReactMapGL>
+                </>
+                : <div>...loading</div>
+            }
         </>
     )
 }
