2f05d8088f6f210eb552de23017c45a71b455116
Jason Moon <jasonmoonjm@gmail.com>
2019-12-10 17:30:45 -0800

    mycheckin component/route made

--- src/components/GotNext/index.js
+++ src/components/GotNext/index.js
@@ -6,6 +6,7 @@ import CourtShow from '../CourtShow';
 import Main from '../Main';
 import ProfileShow from '../ProfileShow';
 import EditProfile from '../EditProfile';
+import MyCheckIn from '../MyCheckIn';
 
 const GotNext = ({ currentUser }) => {
     return (
@@ -14,7 +15,7 @@ const GotNext = ({ currentUser }) => {
             <Switch>
                 <Route exact path={'/main'} render={() => <Main/>}/>
                 <Route exact path={'/main/profile'} render={routeProps => <ProfileShow currentUser={currentUser} {...routeProps}/>}/>
-                <Route exact path={'/main/mycheckin'} render={() => <div>myCheckIn Page</div>}/>
+                <Route exact path={'/main/mycheckin'} render={() => <MyCheckIn currentUser={currentUser}/>}/>
                 <Route exact path={'/main/profile/edit'} render={routeProps => <EditProfile currentUser={currentUser} {...routeProps}/>}/>
                 <Route exact path={'/main/:court'} render={routeProps => <CourtShow currentUser={currentUser} {...routeProps}/>}/>
             </Switch>
---
+++ src/components/MyCheckIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyCheckIn = ({ currentUser }) => {
+    return (
+        <h1>MyCheckIn Page</h1>
+    )
+}
+
+export default MyCheckIn; 
