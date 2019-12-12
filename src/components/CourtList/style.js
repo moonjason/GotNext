3601ad64519574c4719d45f0e063183c634588ab
Jason Moon <jasonmoonjm@gmail.com>
2019-12-11 21:40:20 -0800

    add animations to main component links

--- src/components/CourtList/style.js
+++ src/components/CourtList/style.js
@@ -1,4 +1,13 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`
 
 export const CourtLinkCard = styled.div`
     width: 30rem;
@@ -8,6 +17,8 @@ export const CourtLinkCard = styled.div`
     border-image: linear-gradient(45deg, #FF4600, rgb(250,224,66)) 1;
     text-align: center;
     padding: 1rem 0;
+    animation: ${fadeIn} .5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
+    background-color: rgba(255, 224, 98, .3);
 `
 
 export const CourtCard = styled.div`
